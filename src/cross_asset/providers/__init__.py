5d682a69fc8fdeafb06875809326787341f48752
yjiao4903-lang <yjiao4903@gmail.com>
2026-09-17 05:54:44 +0800

    feat: export monitoring provider adapter

--- src/cross_asset/providers/__init__.py
+++ src/cross_asset/providers/__init__.py
@@ -3,6 +3,7 @@ from .fixture import FixtureProvider
 from .fred import FREDProvider, FredProvider
 from .ifind import IfindProvider, IFINProvider
 from .manual import ManualProvider
+from .monitoring import YahooMonitoringProvider
 from .tushare import TushareProvider
 from .wind import WindProvider
 from .yahoo import YahooProvider
@@ -21,5 +22,6 @@ __all__ = [
     "ProviderError",
     "TushareProvider",
     "WindProvider",
+    "YahooMonitoringProvider",
     "YahooProvider",
 ]
